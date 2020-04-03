c10cf4853991363652489e7c00b1f668c604fd06
xxxretro <59125462+xxxretro@users.noreply.github.com>
2020-04-03 20:23:18 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,25 +20,78 @@ async def on_ready():
 
 
 @bot.command(pass_context=True)
-async def calc(ctx, arg):
-    time = [int(arg.split(':')[0]), int(arg.split(':')[1])]
-    time_table = []
-    for i in range(8):
-        time_table.append(f'{instr(time[0])}:{instr(time[1])}')
-        if time[1] + 24 >= 60:
-            if time[0] + 4 >= 24:
-                time[0] = time[0] + 4 - 24
-            else:
-                time[0] += 4
-            time[1] = time[1] + 24 - 60
+async def calctime(ctx, *arg):
+    arg = list(arg)
+    if arg[0] == '-nights':
+        if len(arg) == 3:
+            time_on_server = [int(arg[1].split(':')[0]), int(arg[1].split(':')[1])]
+            time_in_msc = [int(arg[2].split(':')[0]), int(arg[2].split(':')[1])]
+            if time_on_server[1] != 0:
+                time_in_msc[1] += int(0.6 * (60 - time_on_server[1]) * 0.8)
+                time_on_server[1] = 0
+            time_table = []
+            for i in range(180):
+                if time_on_server[0] == 23:
+                    time_table.append(
+                        f'{instr(time_on_server[0])}:{instr(time_on_server[1])}'
+                        f' - '
+                        f'{instr(time_in_msc[0])}:{instr(time_in_msc[1])}'
+                    )
+                if time_in_msc[1] + 8 >= 60:
+                    time_in_msc[1] = (time_in_msc[1] + 8) - 60
+                    if time_in_msc[0] + 1 == 24:
+                        time_in_msc[0] = 0
+                    else:
+                        time_in_msc[0] += 1
+                else:
+                    time_in_msc[1] += 8
+
+                if time_on_server[0] + 1 == 24:
+                    time_on_server[0] = 0
+                else:
+                    time_on_server[0] += 1
+
+            for i in time_table:
+                await ctx.send(i)
         else:
-            time[1] += 24
-            if time[0] + 3 >= 24:
-                time[0] = time[0] + 3 - 24
-            else:
-                time[0] += 3
-    for i in time_table:
-        await ctx.send(i)
+            await ctx.send('Что-то не то :thinking:\nДля получения справки о командах введите: >>help')
+
+    if arg[0] == '-exp':
+        if len(arg) == 2:
+            time = [int(arg[1].split(':')[0]), int(arg[1].split(':')[1])]
+            time_table = []
+            for i in range(8):
+                time_table.append(f'{instr(time[0])}:{instr(time[1])}')
+                if time[1] + 24 >= 60:
+                    if time[0] + 4 >= 24:
+                        time[0] = time[0] + 4 - 24
+                    else:
+                        time[0] += 4
+                        time[1] = time[1] + 24 - 60
+                else:
+                    time[1] += 24
+                    if time[0] + 3 >= 24:
+                        time[0] = time[0] + 3 - 24
+                    else:
+                        time[0] += 3
+            for i in time_table:
+                await ctx.send(i)
+        else:
+            await ctx.send('Что-то не то :thinking:\nДля получения справки о командах введите: >>help')
+
+
+@bot.command
+def help(ctx):
+    await ctx.send('Команды:'
+                   '\n>>help - справка'
+                   '\n>>calctime - расчёт времени'
+                   '\n   Вариации:'
+                   '\n   -nights [время на сервере] [время в Москве]'
+                   '\n   -exp [время запуска сервера(по МСК)]'
+                   '\n   Примичание:'
+                   '\n      Время на сервере желательно брать время формата: час:00, например 09:00, 17:00,'
+                   ' иначе возникнет погрешность.'
+                   ' Всё время указывается без квадратных скобок и в формате: "часы:минуты".')
 
 
 bot.run(os.environ.get('BOT_TOKEN'))

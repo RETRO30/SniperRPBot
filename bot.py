9e3513be90e0c6b023260b4dc79028568ef25c4c
xxxretro <59125462+xxxretro@users.noreply.github.com>
2020-04-06 00:25:26 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -27,20 +27,33 @@ async def calctime(ctx, *arg):
             time_on_server = [int(arg[1].split(':')[0]), int(arg[1].split(':')[1])]
             time = [int(arg[2].split(':')[0]), int(arg[2].split(':')[1])]
             if time_on_server[0] % 2 == 0:
-                if time[1] + (22 - time_on_server[0]) // 2 * 17 < 60:
-                    time[1] += (22 - time_on_server[0]) // 2 * 17
+                deltatime = ((22 - time_on_server[0]) // 2) * 17
+                h = deltatime // 60
+                m = deltatime % 60
+                if time[0] + h > 24:
+                    time[0] = time[0] + h - 24
                 else:
-                    while time[1] + (22 - time_on_server[0]) // 2 * 17 >= 60:
-                        time[1] = (time[0] + (22 - time_on_server[0]) // 2 * 17) - 60
-                        time[0] += 1
-                time_on_server[0] = 22
+                    time[0] += h
+                if time[1] + m > 60:
+                    time[1] = time[1] + m - 60
+                else:
+                    time[1] += m
+                time_on_server = [22, 0]
             else:
-                if time[1] + (23 - time_on_server[0]) // 2 * 17 < 60:
-                    time[1] += (23 - time_on_server[0]) // 2 * 17
+                deltatime = ((23 - time_on_server[0]) // 2) * 17
+                h = deltatime // 60
+                m = deltatime % 60
+                if time[0] + h > 24:
+                    time[0] = time[0] + h - 24
                 else:
-                    time[1] = (time[0] + (23 - time_on_server[0]) // 2 * 17) - 60
-                time_on_server[0] = 23
-            time_table = [f'{instr(time_on_server[0])}:{instr(time_on_server[1])}']
+                    time[0] += h
+                if time[1] + m > 60:
+                    time[1] = time[1] + m - 60
+                else:
+                    time[1] += m
+                time_on_server = [23, 0]
+
+            time_table = [f'{instr(time_on_server[0])}:{instr(time_on_server[1])}:']
             for i in range(8):
                 time_table.append(f'{i + 1}.{instr(time[0])}:{instr(time[1])}')
                 if time[1] + 24 >= 60:
@@ -94,6 +107,5 @@ async def showhelp(ctx):
                    '\n        Вариации:'
                    '\n            -nights [время на сервере(например: 06:00, 17:00)] [время по МСК]'
                    '\n            -exp [время запуска сервера(по МСК)]')
-
 
 bot.run(os.environ.get('BOT_TOKEN'))

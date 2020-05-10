49ef8d26f36bc74ebee85b45a5efb141dc8c5487
xxxretro <59125462+xxxretro@users.noreply.github.com>
2020-05-11 02:58:25 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -228,6 +228,7 @@ async def notifications():
 @tasks.loop(seconds=30)
 async def notifications2():
     channel = bot.get_channel(707282177833828443)
+    print(f'{dead_time()[0]}:{dead_time()[1]}')
     if dead_time()[0] == '22':
         await channel.send(f'''Быдло, на грузы поедете? Время {dead_time()[0]}:{dead_time()[1]}''')
 
